8a9d8774268347b7cfdc78e0eb9cf100874d5295
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2022-12-24 19:57:28 +0530

    #include<stdio.h> int main() {     int n,k,x,y;     scanf("%d%d%d%d",&n,&k,&x,&y);     int r;     r=x*k;     if(x<y)     printf("%d",r+x*(n-k));     else     printf("%d",r+y*(n-k)); }

---
+++ Lamps.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,x,y;
+    scanf("%d%d%d%d",&n,&k,&x,&y);
+    int r;
+    r=x*k;
+    if(x<y)
+    printf("%d",r+x*(n-k));
+    else
+    printf("%d",r+y*(n-k));
+}
