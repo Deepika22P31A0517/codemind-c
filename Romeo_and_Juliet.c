8fd5b5e65c5cb6cb2958e6af7ff3559ffb3ec271
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2023-01-07 21:03:26 +0530

    #include<stdio.h> int main() {     int a,b,c,z;     scanf("%d%d%d",&a,&b,&c);     z=(5*a+10*b)/c;     printf("%d",z); }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,n;
+    int a,b,c,z;
     scanf("%d%d%d",&a,&b,&c);
-    n=(5*a+10*b)/c;
-    printf("%d",n);
+    z=(5*a+10*b)/c;
+    printf("%d",z);
 }
