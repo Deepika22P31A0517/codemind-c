94bfe49a596da9b289c5e988d8724109ac15c108
Deepika <119730634+Deepika22P31A0517@users.noreply.github.com>
2023-01-14 15:12:33 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='V')     {         printf("Violet");     }     else if(ch=='I')     {         printf("Indigo");     }     else if(ch=='B')     {         printf("Blue");     }     else if(ch=='G')     {         printf("Green");     }     else if(ch=='Y')     {         printf("Yellow");     }     else if(ch=='O')     {         printf("Orange");     }     else if(ch=='R')     {         printf("Red");     }     else     {         printf("-1");     } }

--- Print_the_color_name.c
+++ Print_the_color_name.c
@@ -1,22 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    char c;
-    scanf("%c",&c);
-    if(c=='V')
-    printf("Violet");
-    else if(c=='I')
-    printf("Indigo");
-    else if(c=='B')
-    printf("Blue");
-    else if(c=='G')
-    printf("Green");
-    else if(c=='Y')
-    printf("Yellow");
-    else if(c=='O')
-    printf("Orange");
-    else if(c=='R')
-    printf("Red");
-    else 
-    printf("-1");
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='V')
+    {
+        printf("Violet");
+    }
+    else if(ch=='I')
+    {
+        printf("Indigo");
+    }
+    else if(ch=='B')
+    {
+        printf("Blue");
+    }
+    else if(ch=='G')
+    {
+        printf("Green");
+    }
+    else if(ch=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(ch=='O')
+    {
+        printf("Orange");
+    }
+    else if(ch=='R')
+    {
+        printf("Red");
+    }
+    else
+    {
+        printf("-1");
+    }
 }
